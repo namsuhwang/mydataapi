b99d5c7cf5ffddf31f7e7f61e20086ca5077f255
Hwang Namsu <namsuhwang@naver.com>
2023-01-04 14:21:31 +0900

    최초 생성

---
+++ src/test/java/com/kpcnc/mydataapi/MydataapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpcnc.mydataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydataapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
